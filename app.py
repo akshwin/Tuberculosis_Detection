4b2379b29b270ace9f9c9e304374afbf2716a77a
akshwin <akshwint.2003@gmail.com>
2025-06-24 00:03:34 +0530

    added app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Set page config FIRST
-st.set_page_config(page_title="Alzheimer's MRI Classifier", layout="wide")
+st.set_page_config(page_title="Alzheimer's MRI Classifier")
 
 import numpy as np
 import tensorflow as tf
